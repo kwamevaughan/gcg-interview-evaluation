d788c0fd99d05545ae1c14cde44cbcc287959266
kwamevaughan <kwamevaughan@gmail.com>
2025-03-01 10:22:37 +0000

    removed cpanel integration

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const nextConfig = {
         NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
         SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
     },
-    output: 'export', // Enables static export mode
-    trailingSlash: true, // Optional: ensures URLs have a trailing slash
 };
 
 export default nextConfig;
